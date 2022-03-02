b29d85bd362bac33e3f6791967bbdf13797580ce
ArminL96 <86778566+ArminL96@users.noreply.github.com>
2022-03-02 15:06:46 +0100

    Add files via upload

---
+++ www/header.php
@@ -0,0 +1,6 @@
+<?php
+	function get_the_header() {
+		$header_html = '<div class="header-child1"><a href="#">Shopname</a></div><div class="header-child2"><ul><li><a href="#">Login</a></li><li><a href="#">Profile</a></li><li><a href="#">Cart</a></li></ul></div>';
+		echo($header_html);
+	}
+?>
---
+++ www/index.php
@@ -0,0 +1,16 @@
+<?php
+	require_once 'header.php';
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="styles/style.css">
+		<link rel="stylesheet" href="styles/header_style.css">
+	</head>
+	<body>
+		<header class="header-container">
+			<?php get_the_header(); ?>
+		</header>
+	</body>
+</html>
