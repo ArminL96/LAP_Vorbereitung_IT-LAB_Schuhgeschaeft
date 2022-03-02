0a1f16e1ee8d9a60c09f7ad5b06522a05c010d54
ArminL96 <86778566+ArminL96@users.noreply.github.com>
2022-03-02 15:38:18 +0100

    Add files via upload

--- www/footer.php
+++ www/footer.php
@@ -1,4 +1,11 @@
 <?php
+/*
+	Footer File
+	Function to create the Footer
+	HTML Code written into a Variable
+	Echo the Variable
+*/
+
 	function get_the_footer() {
 		$footer_html = '<div class="container-footer"></div>';
 		echo($footer_html);
