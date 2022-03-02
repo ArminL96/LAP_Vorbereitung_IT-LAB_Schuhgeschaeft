79de65dda7979f27e2f7fa08bc9c049d79c5f393
ArminL96 <86778566+ArminL96@users.noreply.github.com>
2022-03-02 15:34:41 +0100

    Add files via upload

---
+++ www/footer.php
@@ -0,0 +1,6 @@
+<?php
+	function get_the_footer() {
+		$footer_html = '<div class="container-footer"></div>';
+		echo($footer_html);
+	}
+?>
--- www/header.php
+++ www/header.php
@@ -1,4 +1,11 @@
 <?php
+/*
+	Header File
+	Function to create the Header
+	HTML Code written into a Variable
+	Echo the Variable
+*/
+
 	function get_the_header() {
 		$header_html = '<div class="header-child1"><a href="#">Shopname</a></div><div class="header-child2"><ul><li><a href="#">Login</a></li><li><a href="#">Profile</a></li><li><a href="#">Cart</a></li></ul></div>';
 		echo($header_html);
