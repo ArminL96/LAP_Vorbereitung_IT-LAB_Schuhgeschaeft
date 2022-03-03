2ac9fabbfe87d43ade9cdded189f55fd4fd24717
ArminL96 <86778566+ArminL96@users.noreply.github.com>
2022-03-03 16:14:49 +0100

    Add files via upload

--- www/header.php
+++ www/header.php
@@ -7,7 +7,7 @@
 */
 
 	function get_the_header() {
-		$header_html = '<div class="header-child1"><a href="#">Shopname</a></div><div class="header-child2"><ul><li><a href="#">Login</a></li><li><a href="#">Profile</a></li><li><a href="#">Cart</a></li></ul></div>';
+		$header_html = '<div class="header-child1"><a href="index.php">Shopname</a></div><div class="header-child2"><ul><li><a href="login.php">Login</a></li><li><a href="userprofile.php">Profile</a></li><li><a href="#">Cart</a></li></ul></div>';
 		echo($header_html);
 	}
 ?>
