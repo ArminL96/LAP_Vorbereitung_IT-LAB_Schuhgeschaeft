25f2747d667c9bdc9059678c178aac82ee8919e3
Denis Keller <denkel0902@gmail.com>
2022-03-07 11:43:18 +0100

    Changed Database/shoeshop.sql

--- Database/shoeshop.sql
+++ Database/shoeshop.sql
@@ -24,17 +24,134 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `costumer`
+-- Tabellenstruktur für Tabelle `billingadress`
 --
 
-CREATE TABLE `costumer` (
+CREATE TABLE `billingadress` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `adress` varchar(255) NOT NULL,
+  `country` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `zipcode` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `cartitem`
+--
+
+CREATE TABLE `cartitem` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `price` float NOT NULL,
+  `cartId` int(11) NOT NULL,
+  `productId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `Name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `customer`
+--
+
+CREATE TABLE `customer` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `firstName` varchar(255) NOT NULL,
   `lastName` varchar(255) NOT NULL,
   `shippingAddressId` int(11) NOT NULL,
   `billingAdressId` int(11) NOT NULL,
-  `userId` int(11) NOT NULL,
+  `userId` bigint(20) NOT NULL,
   `cartId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `cartId` int(11) NOT NULL,
+  `customerId` int(11) NOT NULL,
+  `billAddId` int(11) NOT NULL,
+  `shipAddId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `product`
+--
+
+CREATE TABLE `product` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `price` float NOT NULL,
+  `size` int(11) NOT NULL,
+  `color` varchar(255) NOT NULL,
+  `categoryId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `returns`
+--
+
+CREATE TABLE `returns` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `orderId` int(11) NOT NULL,
+  `returned` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `shippingadress`
+--
+
+CREATE TABLE `shippingadress` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `adress` varchar(255) NOT NULL,
+  `country` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `zipcode` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `shopingcart`
+--
+
+CREATE TABLE `shopingcart` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `totalPrice` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `user`
+--
+
+CREATE TABLE `user` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `userName` varchar(255) NOT NULL,
+  `passwort` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -42,9 +159,57 @@ CREATE TABLE `costumer` (
 --
 
 --
--- Indizes für die Tabelle `costumer`
+-- Indizes für die Tabelle `billingadress`
 --
-ALTER TABLE `costumer`
+ALTER TABLE `billingadress`
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indizes für die Tabelle `cartitem`
+--
+ALTER TABLE `cartitem`
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indizes für die Tabelle `customer`
+--
+ALTER TABLE `customer`
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indizes für die Tabelle `orders`
+--
+ALTER TABLE `orders`
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indizes für die Tabelle `product`
+--
+ALTER TABLE `product`
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indizes für die Tabelle `returns`
+--
+ALTER TABLE `returns`
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indizes für die Tabelle `shippingadress`
+--
+ALTER TABLE `shippingadress`
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indizes für die Tabelle `shopingcart`
+--
+ALTER TABLE `shopingcart`
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indizes für die Tabelle `user`
+--
+ALTER TABLE `user`
   ADD UNIQUE KEY `id` (`id`);
 
 --
@@ -52,9 +217,57 @@ ALTER TABLE `costumer`
 --
 
 --
--- AUTO_INCREMENT für Tabelle `costumer`
+-- AUTO_INCREMENT für Tabelle `billingadress`
 --
-ALTER TABLE `costumer`
+ALTER TABLE `billingadress`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `cartitem`
+--
+ALTER TABLE `cartitem`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `returns`
+--
+ALTER TABLE `returns`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `shippingadress`
+--
+ALTER TABLE `shippingadress`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `shopingcart`
+--
+ALTER TABLE `shopingcart`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `user`
+--
+ALTER TABLE `user`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 COMMIT;
 
