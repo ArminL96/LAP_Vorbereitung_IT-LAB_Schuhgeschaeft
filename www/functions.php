d214a4be966371cd7400d3ee34d9ab7e8e96037c
Denis Keller <denkel0902@gmail.com>
2022-03-08 15:28:09 +0100

    rewrote login.php to fit requirements for connecting to Database Added SQLCommunicator_Login function in functions.php script.js might not be needed

--- www/functions.php
+++ www/functions.php
@@ -1,3 +1,19 @@
 <?php
 
+
+function SQLCommunicator_Login($uname, $email, $pass) {
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	try {
+		$conn = new PDO("mysql:host=$servername;dbname=shoeshop", $username, $password);
+		
+		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		echo "Connected to Database successfully" ;
+	}
+	catch (PDOExeption $e) {
+		echo "Connection to Database failed" ;
+	}
+}
+
 ?>
--- www/login.php
+++ www/login.php
@@ -27,19 +27,26 @@
 			<div class="login-page-container">
 				<div class="login-container">
 					<h1>Log-in</h1>
-					<div class="label-container">
-						<label for="uname">Username:</label>
-						<input type="text" placeholder="Username" name="uname" id="login-uname" required/>
-					</div>
-					<div class="label-container">
-						<label for="email">Email:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
-						<input type="email" placeholder="Email" name="email" id="login-email" required/>
-					</div>
-					<div class="label-container">
-						<label for="password">Password:&nbsp;</label>
-						<input type="password" placeholder="Password" name="password" id="login-pass" required/>
-					</div>
-					<input type="button" value="Login" onclick="loginFunction()" class="button-style"/>
+					<?php
+						if (isset($_POST['uname']) && isset($_POST['email']) && isset($_POST['password']) && isset($_POST['login'])) {
+							SQLCommunicator_Login($_POST['uname'], $_POST['email'], $_POST['password']);
+						}
+					?>
+					<form method="POST">
+						<div class="label-container">
+							<label for="uname">Username:</label>
+							<input type="text" placeholder="Username" name="uname" id="login-uname" required/>
+						</div>
+						<div class="label-container">
+							<label for="email">Email:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
+							<input type="email" placeholder="Email" name="email" id="login-email" required/>
+						</div>
+						<div class="label-container">
+							<label for="password">Password:&nbsp;</label>
+							<input type="password" placeholder="Password" name="password" id="login-pass" required/>
+						</div>
+							<input type="submit" name="login" value="Login" class="button-style"/>
+					</form>
 				</div>
 			</div>
 		</main>
