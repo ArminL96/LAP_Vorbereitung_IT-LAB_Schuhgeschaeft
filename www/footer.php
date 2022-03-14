4acd50a5fc14c75658c33040616d42bd969f1942
Vanessa Hofferer <95428727+Grunge-Kid@users.noreply.github.com>
2022-03-14 15:58:28 +0100

    Update footer.php

--- www/footer.php
+++ www/footer.php
@@ -1,13 +1,1 @@
-<?php
-/*
-	Footer File
-	Function to create the Footer
-	HTML Code written into a Variable
-	Echo the Variable
-*/
-
-	function get_the_footer() {
-		$footer_html = '<div class="container-footer"></div>';
-		echo($footer_html);
-	}
-?>
+<div class="container-footer"></div>
