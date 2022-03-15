708c38f8c52a09cb9176c76af142e6fa2ef3dde2
Vanessa Hofferer <95428727+Grunge-Kid@users.noreply.github.com>
2022-03-15 14:56:16 +0100

    Update footer.php

--- www/footer.php
+++ www/footer.php
@@ -1,2 +1,10 @@
-#div class for footer
+<!--footer-->
+<html>
+<head>
+  <!--css for footer-->
+	<link rel="stylesheet" href="style/footer_style.css">
+</head>
+<body>
 <div class="container-footer"></div>
+</body>
+</html>
