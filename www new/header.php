c7420184b25b85f63e7acbc86bb71ce1d1e90b7c
Vanessa Hofferer <95428727+Grunge-Kid@users.noreply.github.com>
2022-03-17 12:54:59 +0100

    Add files via upload

---
+++ www new/header.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<link rel="stylesheet" href="style/header_style.css">
+</head>
+<body>
+	<!--Header-->
+	<header class="header-container">
+		<div class='header-child1'>
+				<!--website title with a redirect to product page-->
+			<a href='products.php'>Schuhversand</a>
+		</div>
+	</header>
+</body>
+</html>
