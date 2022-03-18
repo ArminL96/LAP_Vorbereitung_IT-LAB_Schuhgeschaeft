1d9a679b794fc6ae464171df58119b4694ada5e5
Vanessa Hofferer <95428727+Grunge-Kid@users.noreply.github.com>
2022-03-18 07:47:36 +0100

    Update userprofile.php

--- www new/userprofile.php
+++ www new/userprofile.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<link rel="stylesheet" href="style/userprofile_style.css">
+		<link rel="shortcut icon" href="../www/img/favicon.ico" type="image/x-icon">
 </head>
 <body>
 	<!--Include the header.php-->
