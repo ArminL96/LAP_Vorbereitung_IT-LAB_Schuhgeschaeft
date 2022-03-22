46426c84b5b7a883e827d6c66eae48b0a38785cf
Vanessa Hofferer <95428727+Grunge-Kid@users.noreply.github.com>
2022-03-22 16:04:03 +0100

    Update login.php

--- www new/login.php
+++ www new/login.php
@@ -5,21 +5,6 @@
 	<link rel="shortcut icon" href="../www/img/favicon.ico" type="image/x-icon">
 </head>
 <body>
-	<?php
-	#database server data
-	$mysqli = new mysqli("localhost", "root", "", "Schuhgeschaeft");
-	#checks if no connection could be established
-	if($mysqli->connect_error){
-		#if true the connection failed
-		die("connection failed: ".$mysqli->connect_error);
-	}
-	#‎select data form database
-	$sql = "SELECT
-	userName,
-	passwort
-	FROM user";
-	$result = $mysqli->query($sql);
-	?>
 	<div class="login-page-container">
 		<div class="login-container">
 			<h1>Log-in</h1>
@@ -35,6 +20,13 @@
 					<input type="password" placeholder="Password" name="password" id="login-pass" required/>
 				</div>
 				<?php
+				#database server data
+				$mysqli = new mysqli("localhost", "root", "", "Schuhgeschaeft");
+				#checks if no connection could be established
+				if($mysqli->connect_error){
+					#if true the connection failed
+					die("connection failed: ".$mysqli->connect_error);
+				}
 				#session start
 				session_start();
 				#if input username and password is not empty
@@ -45,13 +37,21 @@
 					#Session transfer to userprofile to get username
 					$_SESSION["na"] = $login_username;
 				}
-
 				?>
 				<input type="submit" name="login" value="Login" class="button-style"/>
 				<?php
 				#if button login is klicked
 				if ($_SERVER["REQUEST_METHOD"] == "POST")
 				{
+					#‎select data form database
+					$sql = "SELECT
+					userName,
+					passwort,
+					id
+					#‎select data form user where username like the login username
+					FROM user WHERE userName LIKE'%{$login_username}%'";
+					$result = $mysqli->query($sql);
+
 					#if a username exists in the database
 					if ($result->num_rows > 0)
 					{
@@ -63,7 +63,8 @@
 						if (password_verify($login_password, $hash))
 						{
 							#if password is true then redirects to the products page
-							header('Location: products.php');
+							$_SESSION["userID"] = $row["id"];
+							header('Location: ../www/products.php');
 						}
 						#else output an error
 						else
