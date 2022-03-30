a4c1ae1033cf6d7018e6bcff1a0f999f12f6bbcf
DasChr1s <95428751+DasChr1s@users.noreply.github.com>
2022-03-30 15:31:04 +0200

    add impressum.php

---
+++ www/impressum.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Impressum</title>
+	<meta charset="UTF-8">
+	<link href="style_sheet.css" rel="stylesheet">
+	<style>
+
+	h1, h2, h3, h4, p{
+		text-align: center;
+	}
+	h1{
+		
+	}
+	h2{
+		
+	}
+	h3{
+		
+	}
+	h4{
+		
+	}
+	p{
+		
+	}
+	nav{
+		margin-bottom: 100px;
+	}
+	@media screen and (max-width: 800px)
+	{
+			h1
+		{
+			margin-top: 100px;
+		} 
+		
+	}
+
+	
+	
+	
+	
+	
+	</style>
+
+</head>
+
+
+
+<body>
+<header class="header-container">
+		<?php include "header.php"?>
+		<div class='header-child2'>
+			<ul>
+				<!--Buttons for log out, products, cart-->
+				<li>
+					<a href='login.php'>Log out</a>
+				</li>
+				
+				<li>
+					<a href='userprofile.php'>My Profile</a>
+				</li>
+				
+				<li>
+					<a href='products.php'>Products</a>
+				</li>
+				
+				<li>
+					<a href='cart.php'>Cart</a>
+				</li>
+			</ul>
+		</div>
+	</header>
+
+	<!--Überschrift und Menüleiste-->
+		<h1>Impressum</h1>
+´		
+		<h2>Firma Schuhversand</h2>
+		<center>
+		<img src='img/favicon.ico' align="center" width="100" height="100">
+		</center>
+		<br>
+		<h3>Standort und Inhaber:</h3>
+		<p>Musterstraße 1</p>
+		<p>Musterdorf</p>
+		<p>1110</p>
+		<br>
+		<h4>E-Mail: </h4>
+		<p>max.musterman@gmx.at</p>
+		<br>
+		<br>
+		<h4>Informationen:</h4>
+		<br>
+		<p>Alle verwendeten Shop Bilder der Schuhe stammen von der Webseite: https://www.universal.at/.</p>
+		<p>Diese Bilder werden nicht im Kommerziellen Zweck verwendet und dienen in diesem Beispiel nur als Vorschau.</p>
+		<p>Alle User Profil Bilder wurden selbst mit der Website https://www.ionos.com/tools/favicon-generator erstellt.</p>
+		<br>
+		<h4>Datenschutz</h4>
+		<p>Auf dieser Website werden nur Daten gespeichert die mit diesem Webshop in Verbindung stehen. Es werden keine Daten an dritte weitergegeben.</p>
+		
+
+
+	<footer>
+		<!--the footer-->
+		<div class="container-footer"> 
+		<a href="impressum.php"><p>Impressum</p></a>
+		</div>
+	</footer>
+
+</body>
+
+
+
+
+</html>
